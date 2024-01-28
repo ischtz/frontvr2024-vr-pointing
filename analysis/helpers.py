dfd014fea280f94455d7e35a2cf6763ebfa7fdae
Immo Schuetz <ischtz@posteo.eu>
2024-01-27 22:34:21 -0800

    Add preprocessing and baseline error analysis

---
+++ analysis/helpers.py
@@ -0,0 +1,85 @@
+# Helper functions, not written by myself
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+
+# see https://matplotlib.org/gallery/statistics/confidence_ellipse.html
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    """
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+
+# see https://stackoverflow.com/questions/52088966/nearest-intersection-point-to-many-lines-in-python
+def intersect(P0,P1):
+    """P0 and P1 are NxD arrays defining N lines.
+    D is the dimension of the space. This function 
+    returns the least squares intersection of the N
+    lines from the system given by eq. 13 in 
+    http://cal.cs.illinois.edu/~johannes/research/LS_line_intersect.pdf.
+    """
+    # generate all line direction vectors 
+    n = (P1-P0)/np.linalg.norm(P1-P0,axis=1)[:,np.newaxis] # normalized
+
+    # generate the array of all projectors 
+    projs = np.eye(n.shape[1]) - n[:,:,np.newaxis]*n[:,np.newaxis]  # I - n*n.T
+    # see fig. 1 
+
+    # generate R matrix and q vector
+    R = projs.sum(axis=0)
+    q = (projs @ P0[:,:,np.newaxis]).sum(axis=0)
+
+    # solve the least squares problem for the 
+    # intersection point p: Rp = q
+    p = np.linalg.lstsq(R,q,rcond=None)[0]
+
+    return p
